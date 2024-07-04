2b4ccf7ed38c87e38d5b03c2767c2ffb34fd59f6
PolCarva <pablocarvalhogimenez@gmail.com>
2024-07-04 15:10:15 -0300

    chore: Update DownloadBtn and FiltrosAplicados components

--- components/ui/FiltrosAplicados.jsx
+++ components/ui/FiltrosAplicados.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
+import React from "react";
 
-const FiltrosAplicados = ({ hayFiltrosAplicados, renderAppliedFilters, handleRemoveFilter, className = "" }) => {
-    return (
-        <div className={className}>
-            <div className={`flex items-center justify-between px-5`}>
-                <span className={`p-2 font-bold`}>Filtros aplicados:</span>
+const FiltrosAplicados = ({
+  hayFiltrosAplicados,
+  renderAppliedFilters,
+  handleRemoveFilter,
+  className = "",
+}) => {
+  return (
+    <div className={className}>
+      <div className={`flex pr-1 items-center mb-4 justify-between`}>
+        <span className={`p-2 font-bold`}>Filtros aplicados:</span>
 
-                {hayFiltrosAplicados && (
-                    <button onClick={() => handleRemoveFilter("Todos")}
-                        className="px-2 py-1 float-right h-fit text-sm text-white font-semibold bg-gray-400 rounded-md hover:bg-red-800 transition-all ease-in-out">
-                        Borrar Filtros
-                    </button>
-                )}
-            </div>
-            <div>
-                <div className={`flex px-7 items-baseline justify-start flex-1 gap-2 order-2 md:order-1 ${hayFiltrosAplicados ? "w-full md:w-auto" : "w-auto"}`}>
-                    {hayFiltrosAplicados ? (
-                        <div className="flex flex-wrap gap-2">
-                            {renderAppliedFilters()}
-                        </div>
-                    ) : (
-                        <span className='text-green-800'>Ningún filtro.</span>
-                    )}
-                </div>
-            </div>
+        {hayFiltrosAplicados && (
+          <button
+            onClick={() => handleRemoveFilter("Todos")}
+            className="px-2 py-1 float-right h-fit text-sm text-white font-semibold bg-gray-400 rounded-md hover:bg-red-800 transition-all ease-in-out"
+          >
+            Borrar Filtros
+          </button>
+        )}
+      </div>
+      <div>
+        <div
+          className={`flex px-1 items-baseline justify-start flex-1 gap-2 order-2 md:order-1 ${
+            hayFiltrosAplicados ? "w-full md:w-auto" : "w-auto"
+          }`}
+        >
+          {hayFiltrosAplicados ? (
+            <div className="flex flex-wrap gap-2">{renderAppliedFilters()}</div>
+          ) : (
+            <span className="text-green-800">Ningún filtro.</span>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FiltrosAplicados;
-
