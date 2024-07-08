5c52461de3b4fd2f9d47e52898bf5308cd1a160f
PolCarva <pablocarvalhogimenez@gmail.com>
2024-07-08 11:43:00 -0300

    chore: Update stables API and base URLs

--- stables/stables.jsx
+++ stables/stables.jsx
@@ -5,6 +5,6 @@ const ENV_API_URL = process.env.NEXT_PUBLIC_CMS_URL
 const API_URL = isDevelopment ? ENV_API_URL : ENV_API_URL;
 
 export const stables = {
-    API_URL: API_URL,
-    BASE_URL: "https://becasmeccms.onrender.com/media/"
+    API_URL: `${API_URL}api/`,
+    BASE_URL: `${API_URL}media/`
 }
