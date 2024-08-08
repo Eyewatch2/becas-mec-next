a7ac5b4890c3a6d09fab12c5a08dd0f518a85b4c
PolCarva <pablocarvalhogimenez@gmail.com>
2024-08-08 19:55:23 -0300

    chore: Remove unused domain from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'becas.up.railway.app'],
     remotePatterns: [
       {
         protocol: "https",
