67a541bb4ac36d3c3cd58613978884d4d4091c35
Pablo Carvalho <107940742+PolCarva@users.noreply.github.com>
2024-12-16 13:54:36 -0300

    comment dev line on stables.jsx

--- stables/stables.jsx
+++ stables/stables.jsx
@@ -1,5 +1,5 @@
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+// const isDevelopment = process.env.NODE_ENV === 'development';
 const ENV_API_URL = process.env.NEXT_PUBLIC_CMS_URL
 
 const API_URL = /* isDevelopment ? "http://localhost:3000/" :*/  ENV_API_URL ;
